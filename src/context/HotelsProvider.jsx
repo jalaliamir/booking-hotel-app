657da56d575fc141f7dbc1d5d317dd7bc8c3b45b
Amir jalali <101628628+jalaliamir@users.noreply.github.com>
2025-06-27 12:15:11 +0330

    add context for fetch  hotels

---
+++ src/context/HotelsProvider.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { createContext, useContext } from 'react';
+import useFetch from '../hooks/useFetch';
+import { useSearchParams } from 'react-router-dom';
+
+const HotelContext = createContext();
+
+function HotelsProvider({ children }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const destination = searchParams.get('destination');
+  const room = JSON.parse(searchParams.get('options'))?.room;
+  const { isLoading, data: hotels } = useFetch(
+    'http://localhost:5000/hotels',
+    `q=${destination || ''}&accommodates_gte=${room || 1}`
+  );
+
+  return <HotelContext.Provider value={{ isLoading, hotels }}>{children}</HotelContext.Provider>;
+}
+
+export default HotelsProvider;
+
+export function useHotels() {
+  return useContext(HotelContext);
+}
