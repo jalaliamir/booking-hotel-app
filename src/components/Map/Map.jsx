6ca8df6ea3add40f891e67f01c15dd31815cebc4
Amir jalali <101628628+jalaliamir@users.noreply.github.com>
2025-06-28 07:29:35 +0330

    add geolocation hooks for get current location

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,6 +3,7 @@ import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import { useHotels } from '../../context/HotelsProvider';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import useGeoLocation from '../../hooks/useGeoLocation';
 
 function Map() {
   const { isLoading, hotels } = useHotels();
@@ -10,20 +11,30 @@ function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
+  const { isLoading: isLoadingPosition, position: geoLocationPosition, getPosition } = useGeoLocation();
 
   useEffect(() => {
     if (lat && lng) setMapCenter([lat, lng]);
   }, [lat, lng]);
+
+  useEffect(() => {
+    if (geoLocationPosition?.lat && geoLocationPosition?.lng)
+      setMapCenter([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
+
   return (
     <div className='mapContainer'>
       <MapContainer className='map' center={mapCenter} zoom={13} scrollWheelZoom={true}>
+        <button onClick={getPosition} className='getLocation'>
+          {isLoadingPosition ? 'Loading ...' : 'Use Your Location'}
+        </button>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         />
-        <ChangeCenter position={[lat || 51, lng || 3]} />
+        <ChangeCenter position={mapCenter} />
         {hotels.map((item) => (
-          <Marker key={item.key} position={[item.latitude, item.longitude]}>
+          <Marker key={item.id} position={[item.latitude, item.longitude]}>
             <Popup>{item.host_location}</Popup>
           </Marker>
         ))}
